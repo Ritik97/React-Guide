ee881eda07e8b975b2f8437126ab4c975e6353c9
Ritik Sinha <ritiksinha91@gmail.com>
2020-10-29 08:37:25 +0530

    using React.createRef()

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,20 +5,25 @@ import classes from './Person.css';
 
 class Person extends Component{
 
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.inputElement.focus();
+        this.inputElementRef.current.focus();
     };
 
     render() {
         console.log('[Person.js] render');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.deletePersonHandler}>My name is {this.props.name} </p>
-                <p>My age is {this.props.age} </p>
-                <p> {this.props.children} </p>
-                <input onChange={this.props.nameChangeHandler} 
-                value={this.props.name} type='text' 
-                ref={(inputEle) => this.inputElement = inputEle } ></input>
+            <div className={ classes.Person }>
+                <p onClick={ this.props.deletePersonHandler }>My name is { this.props.name } </p>
+                <p>My age is { this.props.age } </p>
+                <p> { this.props.children } </p>
+                <input onChange={ this.props.nameChangeHandler } 
+                value={ this.props.name } type='text' 
+                ref={ this.inputElementRef } ></input>
             </div>
         );
     };
